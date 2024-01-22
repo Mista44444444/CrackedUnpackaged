c2ddbf2a36612112b9874325a08b2021bc501f61
Spark4444 <102835794+Mista44444444@users.noreply.github.com>
2024-01-22 17:41:50 +0100

    Update main.js

--- IRL-NYC-SIMULATOR/main.js
+++ IRL-NYC-SIMULATOR/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const { app, BrowserWindow, Menu} = require('electron')
 
 let mainWindow;
@@ -33,40 +32,4 @@ app.on('window-all-closed', () => {
 
 app.on('will-quit', () => {
     globalShortcut.unregisterAll()
-=======
-const { app, BrowserWindow, Menu} = require('electron')
-
-let mainWindow;
-
-const createWindow = () => {
-    mainWindow = new BrowserWindow({
-        height: 600,
-        width: 800,
-        icon: __dirname + `img/icon.png`
-    })
-  
-    mainWindow.loadFile('index.html')
-    mainWindow.webContents.openDevTools()
-    mainWindow.maximize()
-}
-
-const template = [
-   
-] 
-
-const menu = Menu.buildFromTemplate(template)
-Menu.setApplicationMenu(menu)
-
-app.whenReady().then(() => {
-    createWindow()
-})
-
-
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
-})
-
-app.on('will-quit', () => {
-    globalShortcut.unregisterAll()
->>>>>>> 8a92bf92fea3dd0587211fb79915da67db5f3e5e
 })
